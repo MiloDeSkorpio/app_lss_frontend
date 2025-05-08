ae252d4fbb16a33b94b4b24e92b9c726e817e62f
unknown <arcadio.2es0@gmail.com>
2025-05-08 13:46:05 -0600

    feat(main) add router

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react"
+import { createRoot } from "react-dom/client"
+import "./index.css"
+import { QueryProvider } from "./providers/queryProvider.tsx"
+import { Router } from "./routes.tsx"
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
+    <QueryProvider>
+      <Router />
+    </QueryProvider>
+  </StrictMode>
 )
