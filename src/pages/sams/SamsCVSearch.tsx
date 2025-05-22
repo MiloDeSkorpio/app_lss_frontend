b10f489667fff60d06f007a8eb5dc66fad4b8bed
unknown <arcadio.2es0@gmail.com>
2025-05-22 17:22:47 -0600

    feat(view): add view for SamsCVSearch

---
+++ src/pages/sams/SamsCVSearch.tsx
@@ -0,0 +1,12 @@
+import Search from "../../components/features/Search"
+
+const SamsCVSearch = () => {
+  return (
+  <>
+    <h1>Buscar SAMS CV</h1>
+    <Search />
+  </>
+)
+}
+
+export default SamsCVSearch
