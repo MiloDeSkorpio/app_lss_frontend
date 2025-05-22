a2a006a53cb4b3b87c7df14339731382e5d373b2
unknown <arcadio.2es0@gmail.com>
2025-05-21 18:04:02 -0600

    feat(routes): add routes csv-update

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,43 +1,45 @@
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import { QueryProvider } from './providers/queryProvider';
-import  SamsView  from './pages/SamsView';
-import App from './App';
-import CardsView from './pages/CardsView';
-import TransactionView from './pages/TransactionView';
-import SecurityView from './pages/SecurityView';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { QueryProvider } from './providers/queryProvider'
+import  SamsView  from './pages/sams/SamsView'
+import App from './App'
+import CardsView from './pages/CardsView'
+import TransactionView from './pages/TransactionView'
+import SecurityView from './pages/SecurityView'
+import SamsCVUpdate from './pages/sams/SamsCVUpdate'
 
 const router = createBrowserRouter([
   {
-    path: "/",
+    path: "",
     element: <App />,
     // errorElement: <ErrorPage />,
   },
   {
-    path: "/sams",
+    path: "sams",
     element: <SamsView />,
-    children: [
-      
-    ]
   },
   {
-    path: "/tarjetas",
+    path: "sams/update-cv",
+    element: <SamsCVUpdate />,
+  },
+  {
+    path: "tarjetas",
     element: <CardsView />,
   },
   {
-    path: "/listas-seguridad",
+    path: "listas-seguridad",
     element: <SecurityView />,
   },
   {
-    path: "/transacciones",
+    path: "transacciones",
     element: <TransactionView />,
   },
   // Puedes agregar más rutas aquí
-]);
+])
 
 export function Router() {
   return (
     <QueryProvider>
       <RouterProvider router={router} />
     </QueryProvider>
-  );
+  )
 }
