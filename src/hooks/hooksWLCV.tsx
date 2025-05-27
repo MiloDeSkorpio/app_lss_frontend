bff6ab954d77a53e5fe810353aeaa8c4bb4f2c1b
unknown <arcadio.2es0@gmail.com>
2025-05-26 18:47:29 -0600

    feat(hooks): add hooks for whitelist and inventory

--- src/hooks/hooksWLCV.tsx
+++ src/hooks/hooksWLCV.tsx
@@ -5,14 +5,34 @@ import apiClient from '../services/apiClient.tsx'
 // Ejemplo para obtener datos (GET)
 export const getLatestVersionWLCV = () => {
   return useQuery({
-    queryKey: ['whitelist'],
+    queryKey: ['whitelistcv'],
     queryFn: async () => {
       const response = await apiClient.get('/whitelist/last-version-cv')
       return response.data
     },
   })
 }
-
+export const getLatestVersionWL = () => {
+  return useQuery({
+    queryKey: ['whitelist'],
+    queryFn: async () => {
+      const response = await apiClient.get('/whitelist/last-version')
+      console.log(response)
+      return response.data
+    },
+  })
+}
+export const getLatestVersionInv = () => {
+  return useQuery({
+    queryKey: ['inventory'],
+    queryFn: async () => {
+      const response = await apiClient.get('/sams/all-records')
+      console.log(response)
+      return response.data
+    },
+  })
+}
+// Ejemplo para enviar datos (POST)
 export const useUploadCV = () => {
   return useMutation({
     mutationFn: async (formData: FormData) => {
