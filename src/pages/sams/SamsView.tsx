4f5a25d1d6d6225638ac3f8742712ad354c7e096
unknown <arcadio.2es0@gmail.com>
2025-05-26 18:48:01 -0600

    feat(component): add components for inventory and whitelist

--- src/pages/sams/SamsView.tsx
+++ src/pages/sams/SamsView.tsx
@@ -1,13 +1,22 @@
 import { useNavigate } from "react-router-dom"
 import OperatorCard from "../../components/common/OperatorCard"
-import { getLatestVersionWLCV } from "../../hooks/hooksWLCV"
+import { getLatestVersionInv, getLatestVersionWL, getLatestVersionWLCV } from "../../hooks/hooksWLCV"
 import { convertToCSV, downloadFile, getCurrentDateTime } from "../../utils/FileHelpers"
 
 const SamsView = () => {
   const navigate = useNavigate()
+  
   const { data: samsCV, isLoading, error } = getLatestVersionWLCV()
   const totalSAMSCV = samsCV?.length || 0
   const lastVersionCV = samsCV?.[0]?.version || "N/A"
+
+  const { data: samsWL} = getLatestVersionWL()
+  const totalSAMS = samsWL?.length || 0
+  const lastVersion = samsWL?.[0]?.version || "N/A"
+
+  const { data: samsInv} = getLatestVersionInv()
+  const totalSAMSInv = samsInv?.length || 0
+  const lastFecha = samsInv?.[0]?.fecha_produccion || "N/A"
 
   const handleDownloadCV = (samsCV: any) => {
     const dateTime = getCurrentDateTime()
@@ -44,8 +53,12 @@ const SamsView = () => {
     downloadFile(hexCSV, `${dateTime}_listablanca_cv.csv`, "text/csv")
   }
 
-  const countOperator = (op: string) =>
+  const countOperatorCV = (op: string) =>
     samsCV?.filter((s: { operator: string }) => s.operator === op).length || 0
+  const countOperator = (op: string) =>
+    samsWL?.filter((s: { operator: string }) => s.operator === op).length || 0
+  const countOperatorInv = (op: string) =>
+    samsInv?.filter((s: { provider_code: string }) => s.provider_code === op).length || 0
 
   if (isLoading) return <div>Loading...</div>
   if (error) return <div>Error: {error.message}</div>
@@ -57,6 +70,35 @@ const SamsView = () => {
           title="Lista Blanca CV"
           lastVersion={lastVersionCV}
           totalRecords={totalSAMSCV}
+          operators={[
+            { label: "STE", value: countOperatorCV("3C") + countOperatorCV("5A") },
+            { label: "STC", value: countOperatorCV("32") },
+            {
+              label: "MB",
+              value:
+                countOperatorCV("01") +
+                countOperatorCV("02") +
+                countOperatorCV("03") +
+                countOperatorCV("04") +
+                countOperatorCV("05") +
+                countOperatorCV("06") +
+                countOperatorCV("07"),
+            },
+            { label: "ORT", value: countOperatorCV("15") },
+            { label: "RTP", value: countOperatorCV("46") },
+          ]}
+          onDownload={(e) => {
+            e.preventDefault()
+            handleDownloadCV(samsCV)
+          }}
+          onUpdate={() => navigate("/sams/update-cv")}
+          onSearch={() => navigate("/sams/search-cv")}
+          onHistory={() => navigate("/sams/versions-cv")}
+        />
+        <OperatorCard
+          title="Lista Blanca"
+          lastVersion={lastVersion}
+          totalRecords={totalSAMS}
           operators={[
             { label: "STE", value: countOperator("3C") + countOperator("5A") },
             { label: "STC", value: countOperator("32") },
@@ -76,57 +118,43 @@ const SamsView = () => {
           ]}
           onDownload={(e) => {
             e.preventDefault()
-            handleDownloadCV(samsCV)
+            handleDownloadCV(samsWL)
           }}
-          onUpdate={() => navigate("/sams/update-cv")}
-          onSearch={() => navigate("/sams/search-cv")}
-          onHistory={() => navigate("/sams/versions-cv")}
+          onUpdate={() => navigate("/sams/update")}
+          onSearch={() => navigate("/sams/search")}
+          onHistory={() => navigate("/sams/versions")}
+        />
+        <OperatorCard
+          title="Inventario de SAMS"
+          lastVersion={lastFecha}
+          totalRecords={totalSAMSInv}
+          operators={[
+            { label: "STE", value: countOperatorInv("3C") + countOperatorInv("5A") },
+            { label: "STC", value: countOperatorInv("32") },
+            {
+              label: "MB",
+              value:
+                countOperatorInv("01") +
+                countOperatorInv("02") +
+                countOperatorInv("03") +
+                countOperatorInv("04") +
+                countOperatorInv("05") +
+                countOperatorInv("06") +
+                countOperatorInv("07"),
+            },
+            { label: "ORT", value: countOperatorInv("15") },
+            { label: "RTP", value: countOperatorInv("46") },
+          ]}
+          onDownload={(e) => {
+            e.preventDefault()
+            handleDownloadCV(samsWL)
+          }}
+          onUpdate={() => navigate("/sams/update")}
+          onSearch={() => navigate("/sams/search")}
+          onHistory={() => navigate("/sams/versions")}
         />
 
-        {/* Este seria el inicio del componente */}
-        <div className=" space-y-1 border-2 border-amber-50 p-4">
-          <h2 className="font-bold">Lista Blanca CL</h2>
-          <div className="flex space-y-2 space-x-3">
-            <div className="">
-              <p>Ultima Version: 2</p>
-              <p>Registros: 14864</p>
-              <p>STE: 14864</p>
-              <p>STC: 14864</p>
-              <p>MB: 14864</p>
-              <p>ORT: 14864</p>
-              <p>RTP: 14864</p>
-              <button>Descargar</button>
-            </div>
-            <div className="flex flex-col space-y-3">
-              <button>Actualizar</button>
-              <button>Buscar</button>
-              <button>Historico</button>
-            </div>
-          </div>
-        </div>
-        {/* Aqui termina el primer componente */}
-        {/* Este seria el inicio del componente */}
-        <div className=" space-y-1 border-2 border-amber-50 p-4">
-          <h2 className="font-bold">Inventario</h2>
-          <div className="flex space-y-2 space-x-3">
-            <div className="">
-              <p>Ultima Version: 2</p>
-              <p>Registros: 14864</p>
-              <p>STE: 14864</p>
-              <p>STC: 14864</p>
-              <p>MB: 14864</p>
-              <p>ORT: 14864</p>
-              <p>RTP: 14864</p>
-              <button>Descargar</button>
-            </div>
-            <div className="flex flex-col space-y-3">
-              <button>Actualizar</button>
-              <button>Buscar</button>
-              <button>Historico</button>
-            </div>
-          </div>
-        </div>
-        {/* Aqui termina el primer componente */}
+        
       </div>
       
     </div>
