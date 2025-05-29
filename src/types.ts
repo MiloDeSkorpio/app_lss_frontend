cf2694cc94412bccd2a60f189756492708e85bb7
unknown <arcadio.2es0@gmail.com>
2025-05-28 18:22:07 -0600

    feat(types): add file to asign types for all project

---
+++ src/types.ts
@@ -0,0 +1,10 @@
+export type SearchResult = {
+  SERIAL_DEC: number;
+  SERIAL_HEX: string;
+  CONFIG: string;
+  OPERATOR: string;
+  LOCATION_ID: string;
+  ESTACION: string;
+  VERSION: string;
+  ESTADO: string;
+};
