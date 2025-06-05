4651a1d831d605047754a3c1da09fc892cc0325d
unknown <arcadio.2es0@gmail.com>
2025-06-05 12:30:03 -0600

    feat(params): add params for perzonalize LoaderCSV

--- src/pages/sams/SamsCVUpdate.tsx
+++ src/pages/sams/SamsCVUpdate.tsx
@@ -1,16 +1,23 @@
-import { useUploadCV } from "../../hooks/SamsHooks"
-import LoaderCSV from "../../components/features/LoaderCSV"
-
+import { useUploadCV } from "../../hooks/SamsHooks";
+import LoaderCSV from "../../components/features/LoaderCSV";
 
 const SamsCVUpdate = () => {
-  const uploadMutation = useUploadCV()
+  const multerOpt = "csvFiles";
+  const maxFiles = 15;
+  const multiple = true;
+  const uploadMutation = useUploadCV();
 
   return (
     <div className="container mx-auto p-4">
       <h1 className="text-2xl font-bold mb-6">Actualización de CV</h1>
-      <LoaderCSV uploadMutation={uploadMutation} />
+      <LoaderCSV
+        uploadMutation={uploadMutation}
+        multerOpt={multerOpt}
+        maxFiles={maxFiles}
+        multiple={multiple}
+      />
     </div>
-  )
-}
+  );
+};
 
-export default SamsCVUpdate
+export default SamsCVUpdate;
