2cf812d02fc03c6222a8dd26d20fcd7e6c8f2d53
unknown <arcadio.2es0@gmail.com>
2025-06-05 12:26:28 -0600

    feat(data): update types and columns in results and add onClean handle for clean results

--- src/components/features/Search.tsx
+++ src/components/features/Search.tsx
@@ -1,28 +1,40 @@
 import type { SearchResult } from "../../types";
 
-
 interface ResComponentsProps {
-  data?: SearchResult;
+  data?: SearchResult | SearchResult[]
   isLoading: boolean;
   error: Error | null;
+  onClean: () => void
 }
 
+const Search = ({ data, isLoading, error, onClean  }: ResComponentsProps) => {
+  const results = Array.isArray(data) ? data : data ? [data] : [];
+  
+  const handleClean = () => {
+    onClean()
+    alert('Limpieza Exitosa...')
+  }
 
-const Search = ({ data, isLoading, error }: ResComponentsProps) => {
   if (isLoading) {
     return <p className="text-gray-500 text-center mt-6">Buscando...</p>;
   }
 
   if (error) {
-    return <p className="text-red-600 text-center mt-6">Error: {error.message}</p>;
+    return (
+      <p className="text-red-600 text-center mt-6">Error: {error.message}</p>
+    );
   }
 
-  if (!data) {
-    return <p className="text-gray-400 text-center mt-6">No hay resultados aún.</p>;
+  if (!results) {
+    return (
+      <p className="text-gray-400 text-center mt-6">No hay resultados aún.</p>
+    );
   }
 
   return (
-    <div className="divide-y divide-gray-200 border rounded overflow-hidden mt-4">
+    <>
+    
+    <div className="divide-y divide-gray-200 border rounded overflow-scroll mt-4">
       <table className="min-w-full">
         <thead className="bg-gray-50">
           <tr>
@@ -33,6 +45,9 @@ const Search = ({ data, isLoading, error }: ResComponentsProps) => {
               Operador
             </th>
             <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+              Location ID
+            </th>
+            <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
               Estación
             </th>
             <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
@@ -41,24 +56,36 @@ const Search = ({ data, isLoading, error }: ResComponentsProps) => {
           </tr>
         </thead>
         <tbody className="bg-white divide-y divide-gray-200">
-          <tr className="hover:bg-gray-50">
-            <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-900">
-              {data.SERIAL_HEX}
-            </td>
-            <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
-              {data.OPERATOR}
-            </td>
-            <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
-              {data.ESTACION}
-            </td>
-            <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
-              {data.ESTADO}
-            </td>
-          </tr>
+          {results.map((item,index) => (
+            <tr key={index} className="hover:bg-gray-50">
+              <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-900">
+                {item.SERIAL_HEX}
+              </td>
+              <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
+                {item.OPERATOR}
+              </td>
+              <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
+                {item.LOCATION_ID}
+              </td>
+              <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
+                {item.ESTACION}
+              </td>
+              <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
+                {item.ESTADO}
+              </td>
+            </tr>
+          ))}
         </tbody>
       </table>
     </div>
+      <button 
+      onClick={handleClean}
+      className="bg-blue-600 text-white px-4 py-2 rounded hover:bg-blue-700 disabled:bg-blue-300"
+      >
+        Limpiar Busqueda
+      </button>
+    </>
   );
 };
 
-export default Search
+export default Search;
