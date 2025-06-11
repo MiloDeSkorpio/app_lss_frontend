52d14cb8c163c44084b09916fb50ca585dfce724
unknown <arcadio.2es0@gmail.com>
2025-06-11 17:59:01 -0600

    feature(type): add new interface for compare versions

--- src/types.ts
+++ src/types.ts
@@ -1,10 +1,14 @@
 export type SearchResult = {
-  SERIAL_DEC: number;
-  SERIAL_HEX: string;
-  CONFIG: string;
-  OPERATOR: string;
-  LOCATION_ID: string;
-  ESTACION: string;
-  VERSION: string;
-  ESTADO: string;
+  SERIAL_DEC: number
+  SERIAL_HEX: string
+  CONFIG: string
+  OPERATOR: string
+  LOCATION_ID: string
+  ESTACION: string
+  VERSION: string
+  ESTADO: string
+}
+export interface CompareVersionsParams {
+  currentVers: number
+  oldVersion: number
 }
