bf707edcaa474d113d039b6efabdb72ac8dd08ee
unknown <arcadio.2es0@gmail.com>
2025-06-12 19:01:38 -0600

    feature(toastify): add the npm install toastify and implement on main file

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,13 @@ import { createRoot } from "react-dom/client"
 import "./index.css"
 import { QueryProvider } from "./providers/queryProvider.tsx"
 import { Router } from "./routes.tsx"
-
+import { ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <QueryProvider>
       <Router />
+       <ToastContainer />
     </QueryProvider>
   </StrictMode>
 )
