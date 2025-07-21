2b20a05291e5214ea5d4467337544a4ee02b0e44
unknown <arcadio.2es0@gmail.com>
2025-07-21 10:24:52 -0600

    fix: new validation onCharge files for reject files with distinct pattern out of defined

--- src/pages/sams/SamsCVUpdate.tsx
+++ src/pages/sams/SamsCVUpdate.tsx
@@ -1,12 +1,16 @@
-import { useUploadCV } from "../../hooks/SamsHooks";
-import LoaderCSV from "../../components/features/LoaderCSV";
+import { useUploadCV } from "../../hooks/SamsHooks"
+import LoaderCSV from "../../components/features/LoaderCSV"
+// import { notify } from "../../utils/notifications"
 
 const SamsCVUpdate = () => {
-  const multerOpt = "csvFiles";
-  const maxFiles = 15;
-  const multiple = true;
-  const uploadMutation = useUploadCV();
-
+  const multerOpt = "csvFiles"
+  const maxFiles = 15
+  const multiple = true
+  const uploadMutation = useUploadCV()
+  // if(uploadMutation.isSuccess) {
+  //   notify.success('Se inserto con epxito')
+  // }
+  // console.log(uploadMutation.data)
   return (
     <div className="container mx-auto p-4">
       <h1 className="text-2xl font-bold mb-6">Actualización de CV</h1>
@@ -17,7 +21,7 @@ const SamsCVUpdate = () => {
         multiple={multiple}
       />
     </div>
-  );
-};
+  )
+}
 
-export default SamsCVUpdate;
+export default SamsCVUpdate
