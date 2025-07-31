b29fd6b7487105fbf0c6ca8d2f8107473364f1d7
unknown <arcadio.2es0@gmail.com>
2025-07-31 11:18:15 -0600

    fix: edit the mutationfucntion to send in props on LoaderCSV upload function to validate function

--- src/pages/sams/SamsCVUpdate.tsx
+++ src/pages/sams/SamsCVUpdate.tsx
@@ -1,16 +1,12 @@
-import { useUploadCV } from "../../hooks/SamsHooks"
+import { useValidateCV } from "../../hooks/SamsHooks"
 import LoaderCSV from "../../components/features/LoaderCSV"
-// import { notify } from "../../utils/notifications"
 
 const SamsCVUpdate = () => {
   const multerOpt = "csvFiles"
   const maxFiles = 15
   const multiple = true
-  const uploadMutation = useUploadCV()
-  // if(uploadMutation.isSuccess) {
-  //   notify.success('Se inserto con epxito')
-  // }
-  // console.log(uploadMutation.data)
+  const uploadMutation = useValidateCV()
+
   return (
     <div className="container mx-auto p-4">
       <h1 className="text-2xl font-bold mb-6">Actualización de CV</h1>
