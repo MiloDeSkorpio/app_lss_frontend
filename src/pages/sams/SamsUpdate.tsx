82140b55a6b523f837979d1f7afbce3216f2c447
unknown <arcadio.2es0@gmail.com>
2025-08-13 14:07:59 -0600

    fix: change the hook update to validate

--- src/pages/sams/SamsUpdate.tsx
+++ src/pages/sams/SamsUpdate.tsx
@@ -1,18 +1,18 @@
 
 import LoaderCSV from "../../components/features/LoaderCSV"
-import { useUploadWL } from "../../hooks/SamsHooks"
+import { useValidate } from "../../hooks/SamsHooks"
 
 const SamsUpdate = () => {
   const multerOpt = "csvFiles"
   const maxFiles = 15
   const multiple = true
-  const uploadMutation = useUploadWL()
+  const validateMutation = useValidate()
 
   return (
     <div className="container mx-auto p-4">
       <h1 className="text-2xl font-bold mb-6">Actualización de CV</h1>
       <LoaderCSV
-        uploadMutation={uploadMutation}
+        validateMutation={validateMutation}
         multerOpt={multerOpt}
         maxFiles={maxFiles}
         multiple={multiple}
