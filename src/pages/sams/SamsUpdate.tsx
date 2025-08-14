2a5f8cd52a92c0348b15a84f8b9a35fa1a80a0d2
unknown <arcadio.2es0@gmail.com>
2025-08-13 18:49:46 -0600

    feat: add the upload wl mutation

--- src/pages/sams/SamsUpdate.tsx
+++ src/pages/sams/SamsUpdate.tsx
@@ -1,18 +1,20 @@
 
 import LoaderCSV from "../../components/features/LoaderCSV"
-import { useValidate } from "../../hooks/SamsHooks"
+import { useUploadWL, useValidate } from "../../hooks/SamsHooks"
 
 const SamsUpdate = () => {
   const multerOpt = "csvFiles"
   const maxFiles = 15
   const multiple = true
   const validateMutation = useValidate()
+  const uploadMutation = useUploadWL()
 
   return (
     <div className="container mx-auto p-4">
       <h1 className="text-2xl font-bold mb-6">Actualización de CV</h1>
       <LoaderCSV
         validateMutation={validateMutation}
+        uploadMutation={uploadMutation}
         multerOpt={multerOpt}
         maxFiles={maxFiles}
         multiple={multiple}
