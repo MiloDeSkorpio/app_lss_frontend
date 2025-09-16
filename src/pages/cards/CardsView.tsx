684f85efff0e7c4c2c57cbb609fab1c06bad2599
Efrain Arcadio <arcadio.2es0@gmail.com>
2025-09-15 18:43:04 -0600

    feat: add a interface to show cards resume of blacklsit

--- src/pages/cards/CardsView.tsx
+++ src/pages/cards/CardsView.tsx
@@ -1,50 +1,33 @@
 import { useNavigate } from "react-router-dom";
-import OperatorCard from "../../components/common/OperatorCard";
 import { useRouteAwareApi } from "../../hooks/useRouteAwareApi";
-import type { CardConfig } from "../../types";
+import type { CardLNConfig } from "../../types";
 import { handleDownloadWL } from "../../utils/FileHelpers";
-interface OperatorCardFromConfigProps {
-  cardConfig: CardConfig
+import { ResumeCard } from "../../components/common/ResumeCard";
+
+interface ResumeCardFromConfigProps {
+  cardLNConfig: CardLNConfig
 }
 
-const OperatorCardFromConfig = ({ cardConfig }: OperatorCardFromConfigProps) => {
+const ResumeCardFromConfig = ({ cardLNConfig }: ResumeCardFromConfigProps) => {
   const navigate = useNavigate()
-  const { data, isLoading, error } = cardConfig.useData()
-  const { total, version } = cardConfig.getSummary(data)
-  console.log(data)
-  // A more sophisticated loading/error handling strategy could be implemented here
-  if (isLoading) return <div>Cargando {cardConfig.titleCard}...</div>
-  if (error) return <div>Error al cargar {cardConfig.titleCard}.</div>
+  const { data, isLoading, error } = cardLNConfig.useData()
+  const { total, version } = cardLNConfig.getSummary(data)
+  
+  if (isLoading) return <div>Cargando {cardLNConfig.titleCard}...</div>
+  if (error) return <div>Error al cargar {cardLNConfig.titleCard}.</div>
 
   return (
-    <OperatorCard
-      title={cardConfig.titleCard}
+    <ResumeCard
+      title={cardLNConfig.titleCard}
       lastVersion={version}
       totalRecords={total}
-      operators={[
-        { label: "STE", value: cardConfig.getOperatorCount(data, "3C") + cardConfig.getOperatorCount(data, "5A") },
-        { label: "STC", value: cardConfig.getOperatorCount(data, "32") },
-        {
-          label: "MB",
-          value:
-            cardConfig.getOperatorCount(data, "01") +
-            cardConfig.getOperatorCount(data, "02") +
-            cardConfig.getOperatorCount(data, "03") +
-            cardConfig.getOperatorCount(data, "04") +
-            cardConfig.getOperatorCount(data, "05") +
-            cardConfig.getOperatorCount(data, "06") +
-            cardConfig.getOperatorCount(data, "07"),
-        },
-        { label: "ORT", value: cardConfig.getOperatorCount(data, "15") },
-        { label: "RTP", value: cardConfig.getOperatorCount(data, "46") },
-      ]}
       onDownload={(e) => {
         e.preventDefault()
-        handleDownloadWL(data, cardConfig.downloadName)
+        handleDownloadWL(data, cardLNConfig.downloadName)
       }}
-      onUpdate={() => navigate(cardConfig.nav.update)}
-      onSearch={() => navigate(cardConfig.nav.search)}
-      onHistory={() => navigate(cardConfig.nav.history)}
+      onUpdate={() => navigate(cardLNConfig.nav.update)}
+      onSearch={() => navigate(cardLNConfig.nav.search)}
+      onHistory={() => navigate(cardLNConfig.nav.history)}
     />
   )
 }
@@ -58,10 +41,10 @@ const CardsView = () => {
     <div className="space-y-4">
       <h1 className="font-bold">{title}</h1>
       <div className="flex flex-wrap sapce-x-2">
-      {(Object.values(cards) as CardConfig[]).map((config) => (
-        <OperatorCardFromConfig
+      {(Object.values(cards) as CardLNConfig[]).map((config) => (
+        <ResumeCardFromConfig
           key={config.titleCard}
-          cardConfig={config}
+          cardLNConfig={config}
         />
       ))}
       </div>
