156d24209827f64a30bf0c7beebdd140b3c07743
unknown <arcadio.2es0@gmail.com>
2025-09-19 18:00:56 -0600

    feat: edit the payload and fix the name of the variable for total records on current version records

--- src/components/common/ShowInfo.tsx
+++ src/components/common/ShowInfo.tsx
@@ -1,16 +1,16 @@
 import { useNavigate } from "react-router-dom"
-import type { validationResult } from "../../types"
+import type { uploadPayload, validationResult } from "../../types"
 import OrganismoRes from "./OrganismoRes"
 import { notify } from "../../utils/notifications"
 import type { UseMutationResult } from "@tanstack/react-query"
-import type { ListCVPayload } from "../../hooks/SamsHooks"
+
 
 interface ShowInfoProps {
   isOpen: boolean
   title?: string
   data: validationResult[]
   onClose: () => void
-  uploadMutation: UseMutationResult<any, unknown, ListCVPayload>
+  uploadMutation: UseMutationResult<any, unknown, uploadPayload>
 }
 // Define any props needed for ShowInfo component
 const ShowInfo: React.FC<ShowInfoProps> = ({ isOpen, title = 'Resumen de Version', data, onClose, uploadMutation }) => {
@@ -24,7 +24,6 @@ const ShowInfo: React.FC<ShowInfoProps> = ({ isOpen, title = 'Resumen de Version
     "ORT": ['15'],
     "RTP": ['46']
   }
-  
   const navigate = useNavigate()
   const totalAltas = result.altasValidas.length
   const totalBajas = result.bajasValidas.length
@@ -39,10 +38,9 @@ const ShowInfo: React.FC<ShowInfoProps> = ({ isOpen, title = 'Resumen de Version
       {
         onSuccess: () => {
           notify.success('Nueva Version Creada con Exito!')
-          navigate('/sams')  // Cambia por la ruta a donde quieras i
+          navigate('/sams')  
         },
         onError: (error) => {
-          // manejar error aquí si quieres
           console.error(error)
         }
       }
@@ -50,7 +48,6 @@ const ShowInfo: React.FC<ShowInfoProps> = ({ isOpen, title = 'Resumen de Version
   }
 
   return (
-
     <div className="fixed inset-0 bg-gray-900/40 bg-opacity-20 backdrop-blur-lg border border-opacity-10 flex items-center justify-center z-50 text-black">
       <div className="bg-white rounded-lg p-6 max-w-2xl w-full shadow-lg overflow-auto max-h-[90vh]">
         <div className="flex justify-center items-center mb-4">
@@ -58,7 +55,7 @@ const ShowInfo: React.FC<ShowInfoProps> = ({ isOpen, title = 'Resumen de Version
         </div>
         <div className="space-y-1.5 my-2">
           <div className="flex justify-center space-x-3">
-            <p>Registros Version <span className="font-semibold">{result.currentVersion}</span>: <span className="font-semibold">{result.currentVersionCount}</span></p>
+            <p>Registros Version <span className="font-semibold">{result.currentVersion}</span>: <span className="font-semibold">{result.currentRecordsCount}</span></p>
             <p>Registros Version <span className="font-semibold">{result.newVersion}</span>: <span className="font-semibold">{result.newRecordsCount}</span></p>
           </div>
           <div className="flex justify-center space-x-3">
@@ -78,7 +75,6 @@ const ShowInfo: React.FC<ShowInfoProps> = ({ isOpen, title = 'Resumen de Version
           ))}
         </div>
         <div className="flex mt-2 justify-between">
-
           {(totalAltas !== 0 || totalBajas !== 0 || totalCambios !== 0) && (
             <div>
               <button
@@ -99,7 +95,5 @@ const ShowInfo: React.FC<ShowInfoProps> = ({ isOpen, title = 'Resumen de Version
     </div>
   )
 }
-
-
 
 export default ShowInfo;
