9a5d6f15d276e2c7b6862205e77afbcc7cb72c1b
unknown <arcadio.2es0@gmail.com>
2025-11-12 17:41:59 -0600

    fix: adjust component for show any structure of db

--- src/components/common/SelectVersion.tsx
+++ src/components/common/SelectVersion.tsx
@@ -1,33 +1,34 @@
-import { type FC } from "react"
-import type { VersionRecords } from "../../types"
+
+import type { FC } from "react"
+import type { VersionLNRecords, VersionRecords } from "../../types"
 
 interface SelectVersionProps {
-  dataVersion?: VersionRecords[]
+  dataVersion?: VersionRecords[] | VersionLNRecords[]
   idName: string
-  value: number
+  value: number | string
   disabled: boolean
   onChange: (e: React.ChangeEvent<HTMLSelectElement>) => void
 }
 
-const SelectVersion: FC<SelectVersionProps> = ({ dataVersion, idName, value,onChange, disabled }) => {
-
+const SelectVersion: FC<SelectVersionProps> = ({ dataVersion, idName, value, onChange, disabled }) => {
   return (
-    <>
-      <select
-        id={idName}
-        value={value}
-        onChange={onChange}
-        disabled={disabled}
-        className="w-[220px] border border-gray-500 rounded-md p-2 disabled:border-gray-400 disabled:text-gray-400"
-      >
-        <option value="">Selecciona una versión</option>
-        {dataVersion?.map((ver) => (
-          <option key={ver.VERSION} value={ver.VERSION}>
-            {ver.VERSION}
+    <select
+      id={idName}
+      value={value}
+      onChange={onChange}
+      disabled={disabled}
+      className="w-[220px] border border-gray-500 rounded-md p-2 disabled:border-gray-400 disabled:text-gray-400"
+    >
+      <option value="">Selecciona una versión</option>
+      {dataVersion?.map((ver, idx) => {
+        const version = (ver as any).VERSION || (ver as any).version_ln
+        return (
+          <option key={idx} value={version}>
+            {version}
           </option>
-        ))}
-      </select>
-    </>
+        )
+      })}
+    </select>
   )
 }
 
