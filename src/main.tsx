25eee1de6d992a041f7dccefd4ae70891dc7b766
unknown <arcadio.2es0@gmail.com>
2025-11-26 18:26:49 -0600

    feat: add AuthProvider for authenticate all routes

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,19 @@
 import { StrictMode } from "react"
 import { createRoot } from "react-dom/client"
 import "./index.css"
+import "./App.css"
 import { QueryProvider } from "./providers/queryProvider.tsx"
 import { Router } from "./routes.tsx"
 import { ToastContainer } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
+import { AuthProvider } from "./providers/AuthProvider.tsx"
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <QueryProvider>
-      <Router />
-       <ToastContainer />
+      <AuthProvider>
+        <Router />
+      </AuthProvider>
+      <ToastContainer />
     </QueryProvider>
   </StrictMode>
 )
