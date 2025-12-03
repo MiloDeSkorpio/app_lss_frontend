2da51f245891f1a537d37b72a8e48ef3ae66c75a
unknown <arcadio.2es0@gmail.com>
2025-12-03 16:54:55 -0600

    feat: add hooks for reset password and resed code for email verification

--- src/hooks/useAuth.ts
+++ src/hooks/useAuth.ts
@@ -1,5 +1,6 @@
 import { useQuery, useMutation } from "@tanstack/react-query"
 import apiClient from "../services/apiClient"
+import type { RequestPayload, ResendPayload, ResetPayload, VerifyPayload } from "../types"
 
 export const useMe = () => {
   return useQuery({
@@ -40,5 +41,54 @@ export const useRegister = () => {
       const res = await apiClient.post("/auth/register", payload)
       return res.data
     },
+  })
+}
+
+export const useVerifyEmail = () => {
+  return useMutation({
+    mutationFn: async (payload: VerifyPayload) => {
+      const { data } = await apiClient.post("/auth/verify", payload)
+      return data
+    }
+  })
+}
+
+// Hook para reenviar el código
+export const useResendCode = () => {
+  return useMutation({
+    mutationFn: async (payload: ResendPayload) => {
+      const { data } = await apiClient.post("/auth/resend-code", payload)
+      return data
+    }
+  })
+}
+
+// Hook para solicitar código de reseteo
+export const useRequestPasswordReset = () => {
+  return useMutation({
+    mutationFn: async (payload: RequestPayload) => {
+      const { data } = await apiClient.post("/auth/request-reset", payload)
+      return data
+    }
+  })
+}
+
+// Hook para verificar código recibido
+export const useVerifyResetCode = () => {
+  return useMutation({
+    mutationFn: async (payload: VerifyPayload) => {
+      const { data } = await apiClient.post("/auth/verify-reset", payload)
+      return data
+    }
+  })
+}
+
+// Hook para cambiar la contraseña
+export const useResetPassword = () => {
+  return useMutation({
+    mutationFn: async (payload: ResetPayload) => {
+      const { data } = await apiClient.post("/auth/reset-password", payload)
+      return data
+    }
   })
 }
