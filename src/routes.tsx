acd070925b373156d7e496cb81ce5d47f8528719
unknown <arcadio.2es0@gmail.com>
2025-12-04 14:32:11 -0600

    feat: add publicRoute for auth routes

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,5 +1,15 @@
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import { QueryProvider } from './providers/queryProvider'
+import { AuthProvider } from './providers/AuthProvider'
+import DashboardLayout from './components/layouts/DashboardLayout'
+import PrivateRoute from './components/common/PrivateRoute'
+import PublicRoute from './components/common/PublicRoute'
+import LoginView from './pages/LoginView'
+import RegisterView from './pages/RegisterView'
+import VerifyEmailView from './pages/VerifyEmailView'
+import ResendCodeView from './pages/ResendCodeView'
+import ResetPasswordView from './pages/ResetPasswordView'
+import DashboardHome from './pages/DashboardHome'
 import SamsView from './pages/sams/SamsView'
 import CardsView from './pages/cards/CardsView'
 import TransactionView from './pages/TransactionView'
@@ -7,31 +17,27 @@ import SecurityView from './pages/SecurityView'
 import GenericSearchView from './pages/sams/GenericSearchView'
 import GenericVersionsView from './pages/sams/GenericVersionsView'
 import GenericUpdateView from './pages/sams/GenericUpdateView'
-import { AuthProvider } from './providers/AuthProvider'
-import LoginView from './pages/LoginView'
-import DashboardHome from './pages/DashboardHome'
-import DashboardLayout from './components/layouts/DashboardLayout'
-import PrivateRoute from './components/common/PrivateRoute'
-import RegisterView from './pages/RegisterView'
-import VerifyEmailView from './pages/VerifyEmailView'
-import ResendCodeView from './pages/ResendCodeView'
-import ResetPasswordView from './pages/ResetPasswordView'
 
 const router = createBrowserRouter([
-  { path: "/login", element: <LoginView /> },
-  { path: "/register", element: <RegisterView /> },
-  { path: "/verify-account", element: <VerifyEmailView /> },
-  { path: "/resend-code-account", element: <ResendCodeView /> },
-  { path: "/recuperar-password", element: <ResetPasswordView /> },
-
   {
-    element: <PrivateRoute />,
+    element: <PublicRoute />, 
+    children: [
+      { path: "/login", element: <LoginView /> },
+      { path: "/register", element: <RegisterView /> },
+      { path: "/verify-account", element: <VerifyEmailView /> },
+      { path: "/resend-code-account", element: <ResendCodeView /> },
+      { path: "/recuperar-password", element: <ResetPasswordView /> },
+    ]
+  },
+  {
+    element: <PrivateRoute />, 
     children: [
       {
         path: "/",
         element: <DashboardLayout />,
         children: [
-          { path: "", element: <DashboardHome /> },
+          { index: true, element: <DashboardHome /> },
+          // Rutas SAMS
           { path: "sams", element: <SamsView /> },
           { path: "sams/update-cv", element: <GenericUpdateView /> },
           { path: "sams/update", element: <GenericUpdateView /> },
@@ -42,10 +48,12 @@ const router = createBrowserRouter([
           { path: "sams/versions-cv", element: <GenericVersionsView /> },
           { path: "sams/versions", element: <GenericVersionsView /> },
           { path: "sams/versions-inv", element: <GenericVersionsView /> },
+          // Rutas Tarjetas
           { path: "tarjetas", element: <CardsView /> },
           { path: "tarjetas/update-ln", element: <GenericUpdateView /> },
           { path: "tarjetas/search-card", element: <GenericSearchView /> },
           { path: "tarjetas/versions", element: <GenericVersionsView /> },
+          // Otras
           { path: "listas-seguridad", element: <SecurityView /> },
           { path: "transacciones", element: <TransactionView /> },
         ],
