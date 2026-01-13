ca73b72a797fe04f71c6ac76f0018023a6c318a5
unknown <arcadio.2es0@gmail.com>
2026-01-13 13:24:06 -0600

    fix: adjust the UI for new data estrucure

--- src/pages/sams/SamsView.tsx
+++ src/pages/sams/SamsView.tsx
@@ -4,45 +4,26 @@ import { handleDownloadWL } from "../../utils/FileHelpers"
 import { useRouteAwareApi } from "../../hooks/useRouteAwareApi"
 import type { CardConfig } from "../../types"
 
-// Define the props type for the sub-component
 interface OperatorCardFromConfigProps {
   cardConfig: CardConfig
 }
 
-// Sub-component to manage state and logic for each card from the configuration
 const OperatorCardFromConfig = ({ cardConfig }: OperatorCardFromConfigProps) => {
   const navigate = useNavigate()
   const { data, isLoading, error } = cardConfig.useData()
-  const { total, version } = cardConfig.getSummary(data)
 
-  // A more sophisticated loading/error handling strategy could be implemented here
   if (isLoading) return <div>Cargando {cardConfig.titleCard}...</div>
   if (error) return <div>Error al cargar {cardConfig.titleCard}.</div>
+
   return (
     <OperatorCard
       title={cardConfig.titleCard}
-      lastVersion={version}
-      totalRecords={total}
-      operators={[
-        { label: "STE", value: cardConfig.getOperatorCount(data, "3C") + cardConfig.getOperatorCount(data, "5A") },
-        { label: "STC", value: cardConfig.getOperatorCount(data, "32") },
-        {
-          label: "MB",
-          value:
-            cardConfig.getOperatorCount(data, "01") +
-            cardConfig.getOperatorCount(data, "02") +
-            cardConfig.getOperatorCount(data, "03") +
-            cardConfig.getOperatorCount(data, "04") +
-            cardConfig.getOperatorCount(data, "05") +
-            cardConfig.getOperatorCount(data, "06") +
-            cardConfig.getOperatorCount(data, "07"),
-        },
-        { label: "ORT", value: cardConfig.getOperatorCount(data, "15") },
-        { label: "RTP", value: cardConfig.getOperatorCount(data, "46") },
-      ]}
+      lastVersion={data.version}
+      totalRecords={data.totalRecords}
+      operators={data.recordsByOrg}
       onDownload={(e) => {
         e.preventDefault()
-        handleDownloadWL(data, cardConfig.downloadName,version)
+        handleDownloadWL(data, cardConfig.downloadName, data.version)
       }}
       onUpdate={() => navigate(cardConfig.nav.update)}
       onSearch={() => navigate(cardConfig.nav.search)}
